4d040510cd0e96d94fba009f284385de2d5f648f
Sam Gillingham <gillingham.sam@gmail.com>
2022-08-05 09:52:21 +1000

    try adding --single-version-externally-managed

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -6,5 +6,6 @@ $PYTHON -m pip install --no-deps --ignore-installed . \
         --global-option build_ext \
         --global-option "-I$INCLUDE_PATH" \
         --global-option "-L$LIBRARY_PATH"
+        --single-version-externally-managed --record record.txt
 
 popd
