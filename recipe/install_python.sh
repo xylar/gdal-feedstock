39db34393d325224b36de6cab79928aa54680959
Sam Gillingham <gillingham.sam@gmail.com>
2022-08-11 17:31:08 +1000

    try reconfiguring cmake with Python bindings for Python install step

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-pushd build/swig/python
+# now re-configure with BUILD_PYTHON_BINDINGS:BOOL=ON
+cd build
+cmake -G "Unix Makefiles" \
+      ${CMAKE_ARGS} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_SHARED_LIBS=ON \
+      -DPython_EXECUTABLE="$PYTHON" \
+      -DBUILD_PYTHON_BINDINGS:BOOL=ON \
+      -DBUILD_JAVA_BINDINGS:BOOL=OFF \
+      -DBUILD_CSHARP_BINDINGS:BOOL=OFF \
+      ${SRC_DIR}
+
+
+pushd swig/python
 
 $PYTHON -m pip install --no-deps --ignore-installed . \
         --global-option build_ext \
