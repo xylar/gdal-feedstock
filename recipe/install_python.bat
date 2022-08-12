123ea072567ef9abc7414c1e0a7b80ee012ffcb2
Sam Gillingham <gillingham.sam@gmail.com>
2022-08-12 15:33:58 +1000

    try Windows with same technique

--- recipe/install_python.bat
+++ recipe/install_python.bat
@@ -1,14 +1,37 @@
-set LIBRARY_INC=%PREFIX%\Library\include
-set LIBRARY_LIB=%PREFIX%\Library\lib
 
-pushd build\swig\python
+mkdir pybuild
+if errorlevel 1 exit 1
+cd pybuild
+if errorlevel 1 exit 1
 
-%PYTHON% setup.py build_ext ^
-        --include-dirs %LIBRARY_INC% ^
-        --library-dirs %LIBRARY_LIB% ^
-        --gdal-config gdal-config
-%PYTHON% setup.py build_py
-%PYTHON% setup.py build_scripts
-%PYTHON% setup.py install
+if  %vc% LEQ 9 set MSVC_VER=1500
+if  %vc% GTR 9 set MSVC_VER=1900
 
-popd
+if  %vc% LEQ 9 set MSVC_TS_VER=90
+if  %vc% GTR 9 set MSVC_TS_VER=140
+
+cmake -G "Ninja" ^
+      "%CMAKE_ARGS%" ^
+      -DMSVC_VERSION="%MSVC_VER%" ^
+      -DMSVC_TOOLSET_VERSION="%MSVC_TS_VER%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DGDAL_PYTHON_INSTALL_PREFIX:PATH="%STDLIB_DIR%\.." ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DPython_EXECUTABLE="%PYTHON%" ^
+      -DBUILD_PYTHON_BINDINGS:BOOL=OFF ^
+      -DBUILD_JAVA_BINDINGS:BOOL=OFF ^
+      -DBUILD_CSHARP_BINDINGS:BOOL=OFF ^
+      -DGDAL_USE_KEA:BOOL=ON ^
+      -DKEA_INCLUDE_DIR:PATH="%LIBRARY_INC%" ^
+      -DKEA_LIBRARY:PATH="%LIBRARY_LIB%\libkea.lib" ^
+      -DGDAL_USE_MYSQL:BOOL=OFF ^
+      "%SRC_DIR%"
+
+if errorlevel 1 exit /b 1
+
+cd %SRC_DIR%\swig\python
+if errorlevel 1 exit /b 1
+%PYTHON% %SRC_DIR%\pybuild\swig\python\setup.py install
+if errorlevel 1 exit /b 1
